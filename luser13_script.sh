e8ac9dd2debd144301cc336f97fc19649b4104f2
A-khateeb <khateebafeef@gmail.com>
2018-07-28 22:22:28 +0300

    Add update to Function

--- luser13_script.sh
+++ luser13_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 log() {
-	#This function sends a mesage to SYSLOG and to standard output if verbose is true 
+	#This function sends a mesage to SYSLOG and to standard output if verbose is true
 	local MESSAGE="${@}"
 	if [[ "${VERBOSE}" = 'true' ]]
 	then
@@ -8,6 +8,25 @@ log() {
 	fi
 	logger -t luser13_script.sh "${MESSAGE}"
 }
+backup_FILE(){
+	local FILE="${1}"
+	if [[ -f "${FILE}" ]]
+	then
+		local backup_FILE="/var/tmp/$(basename $FILE).$(date +%F-%N)"
+		log "backing up ${FILE} to ${backup_FILE}"
+		cp -p ${FILE} ${backup_FILE}
+	else
+		return 1
+	fi
+}
 readonly VERBOSE="true"
 log "Hi"
 log "This is fun"
+backup_FILE '/etc/passwd'
+if [[ "${?}" -eq "0" ]]
+then
+	log "File backup succeeded"
+else
+	log "File backup failed"
+	exit 1
+fi
